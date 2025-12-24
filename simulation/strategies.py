4aa6249d199a29ed00399157e9d1f2726a0d9e3c
Liat-h-technion <liat.harell@campus.technion.ac.il>
2025-12-24 23:36:55 +0200

    Added Algorithm 3.
    * Implemented the protocol for algorithm 3 from the article.
    
    * Added "data" field to Process, and used it in the handle_message function of the protocol, to enable accessing and updating the process state.
    
    * Added a "print_decision" method to Protocol class, to enable printing the final decision in consensus algorithms.

--- simulation/strategies.py
+++ simulation/strategies.py
@@ -12,7 +12,7 @@ class RespondToAllProtocol(Protocol):
     Upon receiving any message, broadcast a new message to every other process.
     """
 
-    def handle_message(self, my_pid: int, msg: Message, n: int) -> List[Tuple[int, Any]]:
+    def handle_message(self, my_pid: int, process_data: dict, msg: Message, n: int) -> List[Tuple[int, Any]]:
         responses = []
         # Broadcast to everyone except myself
         for target_id in range(n):
@@ -26,7 +26,7 @@ class RandomSingleMessageProtocol(Protocol):
     Upon receiving any message, sends a new message to a single process, chosen randomly with uniform probability.
     """
 
-    def handle_message(self, my_pid: int, msg: Message, n: int) -> List[Tuple[int, Any]]:
+    def handle_message(self, my_pid: int, process_data: dict, msg: Message, n: int) -> List[Tuple[int, Any]]:
         responses = []
         candidates = [pid for pid in range(n) if pid != my_pid]
         if candidates:
@@ -37,7 +37,7 @@ class RandomSingleMessageProtocol(Protocol):
 
 
 class PingPongProtocol(Protocol):
-    def handle_message(self, my_pid: int, msg: Message, network_n: int) -> List[Tuple[int, Any]]:
+    def handle_message(self, my_pid: int, process_data: dict, msg: Message, n: int) -> List[Tuple[int, Any]]:
         # Responds only to the original sender, and only if the sender's message isn't a response.
         # In this protocol, each pair of nodes will communicate only once as request-response.
         if "RESPONSE" not in str(msg.content):
@@ -46,7 +46,7 @@ class PingPongProtocol(Protocol):
 
 
 class RespondToSenderProtocol(Protocol):
-    def handle_message(self, my_pid: int, msg: Message, network_n: int) -> List[Tuple[int, Any]]:
+    def handle_message(self, my_pid: int, process_data: dict, msg: Message, n: int) -> List[Tuple[int, Any]]:
         return [(msg.sender_id, f"Response from {my_pid}")]
 
 
@@ -63,13 +63,13 @@ class CommitteeProtocol(Protocol):
         """
         self.committee_ids = committee_ids
 
-    def handle_message(self, my_pid: int, msg: Message, network_n: int) -> List[Tuple[int, Any]]:
+    def handle_message(self, my_pid: int, process_data: dict, msg: Message, n: int) -> List[Tuple[int, Any]]:
         responses = []
 
         if my_pid in self.committee_ids:
             # The process is a Committee Member.
             # Behavior: Broadcast to everyone (except myself).
-            for target_id in range(network_n):
+            for target_id in range(n):
                 if target_id != my_pid:
                     responses.append((target_id, f"Committee Broadcast from {my_pid}"))
 
@@ -78,6 +78,118 @@ class CommitteeProtocol(Protocol):
             # Behavior: Reply only to committee members.
             for target_id in self.committee_ids:
                 responses.append((target_id, f"User Report from {my_pid} to Committee"))
+
+        return responses
+
+
+class Algorithm3Protocol(Protocol):
+    def __init__(self, f, R):
+        self.f = f
+        self.R = R
+
+    def initialize_process_data(self) -> dict:
+        """
+        Create The process initial data dictionary (initial state), with relevant keys:
+        phase: The phase number between 1 and f+1. After f+1 phases, the process decides and terminates.
+        round: The round number between 1 and R. After R rounds, the process starts a new phase.
+        v_map: Map from processes to signed values. The key is the origin pid.
+        phase_round_senders: A dictionary mapping (phase, round) to a set of processes that delivered me messages in
+            that phase and round. This is used to keep track of the "valid" number in the algorithm for the current
+            round and for future rounds.
+            We assume that messages received "too early" are kept for future handling. This assumption was not mentioned
+            explicitly in the article, but is needed to ensure termination.
+        decided: Boolean, true if the process has decided and otherwise false.
+        final_v: The value that the process decided on. While not decided, final_v = -1.
+        """
+        return {
+            "phase": 1,
+            "round": 1,
+            "v_map": {},
+            "phase_round_senders": dict(),  # {(phase, round): set of sender pids}
+            "decided": False,
+            "final_v": -1
+        }
+
+    def print_decision(self, pid: int, process_data: dict):
+        print(f"Process {pid} decided {process_data['final_v']}")
+
+    def handle_message(self, my_pid: int, process_data: dict, msg: Message, n: int) -> List[Tuple[int, Any]]:
+        """
+            The handling logic of Algorithm 3 - Binary Byzantine consensus for n = 2f + 1.
+            In this protocol, messages content is a dict with keys:
+                phase: The phase in which the message was sent
+                round: The round in which the message was sent
+                v_map: The v_map of the process that sent the message, updated to when it was sent.
+
+            We check the following conditions on the received v_map:
+                1. My process have not yet signed a value from the origin process.
+                2. The value is already signed by at least p distinct processes, in current phase p.
+            If the conditions hold, the process will sign the value and add it to its v_map.
+            After hearing from (n - f) distinct processes in the same round and phase (meaning valid = n - f), we
+            advance to the next round. When starting a new round, the process will broadcast its v_map to all others.
+            There are f+1 phases in total, and R rounds in each phase.
+            After the final phase finishes, the process decides and terminates.
+        """
+        print(f"{msg.sender_id} -> {my_pid}, msg content: {msg.content}")
+        # If the process has already decided, it will not handle new messages.
+        if process_data.get('decided', False):
+            return []
+
+        received_v_map = msg.content.get('v_map')
+        msg_phase = msg.content.get('phase')
+        msg_round = msg.content.get('round')
+
+        curr_phase = process_data['phase']
+        curr_round = process_data['round']
+
+        for origin_pid, (v, signatures) in received_v_map.items():
+            if len(signatures) >= curr_phase and origin_pid not in process_data["v_map"]:
+                new_signatures = signatures.copy()
+                new_signatures.add(my_pid)
+                process_data['v_map'][origin_pid] = (v, new_signatures)
+                print(f"Process {my_pid} signed on value {v} with origin {origin_pid}")
+
+        if msg_phase < curr_phase or (msg_phase == curr_phase and msg_round < curr_round):
+            #  Message is from a previous phase / round.
+            #  This if statement is for efficiency, to avoid saving past data
+            return []
+
+        if (msg_phase, msg_round) not in process_data['phase_round_senders']:
+            process_data['phase_round_senders'][(msg_phase, msg_round)] = set()
+        process_data['phase_round_senders'][(msg_phase, msg_round)].add(msg.sender_id)
+
+        # valid is the amount of messages from distinct processes, sent in the current phase and round
+        valid = len(process_data['phase_round_senders'][(curr_phase, curr_round)])
+
+        responses = []
+        if valid >= (n - self.f):
+            print(f"Process {my_pid} finished round {curr_round} in phase {curr_phase}")
+            # Starting a new round.
+            del process_data['phase_round_senders'][(curr_phase, curr_round)]  # Delete unnecessary data
+            if curr_round < self.R:
+                process_data['round'] += 1
+            else:
+                # After R rounds, starting a new phase / terminating when last phase ends.
+                process_data['round'] = 1
+                if curr_phase < (self.f + 1):
+                    process_data['phase'] += 1
+                else:
+                    process_data['decided'] = True
+
+                    values = [val for val, sigs in process_data['v_map'].values()]
+                    process_data['final_v'] = max(set(values), key=values.count)
+                    print(f"Process {my_pid} decided on value {process_data['final_v']}")
+                    return []
+
+            # Broadcast my v_map to everyone at the start of a new round.
+            new_msg_content = {
+                "v_map": process_data['v_map'],
+                "round": process_data['round'],
+                "phase": process_data['phase']
+            }
+            for target_id in range(n):
+                if target_id != my_pid:
+                    responses.append((target_id, new_msg_content))
 
         return responses
 
@@ -204,3 +316,31 @@ class CommitteeTrafficGenerator(TrafficGenerator):
 
         else:
             raise ValueError(f"Invalid mode '{self.mode}'. Expected 'all-to-committee' or 'committee-to-all'.")
+
+
+class Algorithm3TrafficGenerator(TrafficGenerator):
+    def __init__(self, input_strategy="random"):
+        self.input_strategy = input_strategy
+
+    def generate(self, network: Network):
+        for sender_pid in range(network.n):
+            process = network.processes[sender_pid]
+
+            if self.input_strategy == "random":
+                v = random.choice([0, 1])
+            else:
+                v = self.input_strategy
+
+            # Add the value v with sender_pid as the origin id and self signature
+            process.data["v_map"][sender_pid] = (v, {sender_pid})
+            print(f"Process {sender_pid} initial input is {v}")
+
+            content = {
+                "v_map": process.data["v_map"],
+                "round": process.data["round"],
+                "phase": process.data["phase"]
+            }
+
+            for receiver_pid in range(network.n):
+                if sender_pid != receiver_pid:
+                    network.create_initial_message(sender_pid, receiver_pid, content)
